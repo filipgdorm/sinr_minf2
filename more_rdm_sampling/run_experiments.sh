e1a0aeb70e62aad8383dee13b543a9fe988b3e0f
filipgdorm <filip.dorm1@gmail.com>
2024-07-31 17:30:00 +0200

    rdm sampling with 5 models

--- more_rdm_sampling/run_experiments.sh
+++ more_rdm_sampling/run_experiments.sh
@@ -7,12 +7,10 @@ size_factors=(1.0 5.0 10.0)
 model_type=("an_full_1000" "an_slds_1000" "an_ssdl_1000")
 
 for size_factor in "${size_factors[@]}"; do
-    mkdir -p "rdm_factor_${size_factor}"
-
     for ((i=0; i<${#model_type[@]}; i++)); do
         # Create dir
-        mkdir -p "${model_type[i]}"
-        o_result_dir="rdm_factor_${size_factor}/${model_type[i]}"
+        result_dir="rdm_factor_${size_factor}/${model_type[i]}"
+        mkdir -p $result_dir
         # Initialize counter
         counter=0
         # Directory containing the model files
@@ -24,8 +22,8 @@ for size_factor in "${size_factors[@]}"; do
                 for MODEL_PATH in "$SUBDIR"/*; do
                     if [ -f "$MODEL_PATH" ]; then  # Check if it is a file
                         # Run the Python script with the current model path and counter
-                        python rdm_sampling.py --size_factor "${size_factor}" --model_path "${MODEL_PATH}" --result_dir "${o_result_dir}" --counter "${counter}"           
-                        python evaluation.py --size_factor "${size_factor}" --model_path "${MODEL_PATH}" --result_dir "${o_result_dir}" --counter "${counter}"
+                        python rdm_sampling.py --size_factor "${size_factor}" --model_path "${MODEL_PATH}" --result_dir "${result_dir}" --counter "${counter}"           
+                        python evaluation.py --size_factor "${size_factor}" --model_path "${MODEL_PATH}" --result_dir "${result_dir}" --counter "${counter}"
                         # Increment the counter
                         ((counter++))
                     fi
